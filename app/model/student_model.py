5bd5bc60d3eaa2f8ea87c82f2d30e6b5b4026d87
Sushilkumar Patil (G10X <sushilkumar.patil@g10x.com>
2026-02-16 20:38:13 +0530

    Schema class for studnets defined

--- app/model/student_model.py
+++ app/model/student_model.py
@@ -1,12 +1,15 @@
 from sqlalchemy import Column, String, Integer, DateTime
 
-class Student:
+class Student(Base):
+    
+    __tablename__ = "Students"
+    
     id = Column("id", Integer, autoincrement=True)
     first_name = Column(String(225), nullable=False)
     last_name = Column(String(225), nullable=False)
     city = Column(String(225), nullable=False)
-    email = Column(String(225), nullable=False)
-    phone = Column(Integer, nullable=False)
+    email = Column(String(225), Unique=True, nullable=False)
+    phone = Column(Integer, Unique=True, nullable=False)
     dob = Column(DateTime, nullable=False)
     
     
---
+++ app/schema/student_schema.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+from typing import EmailStr
+
+class Student(BaseModel):
+    """Student BaseModel to create a student
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+    first_name: str = Field(description="Enter first name of student")
+    last_name: str = Field(description="Enter last name of student")
+    city: str = Field(description="Enter your city name")
+    email: str = Field(description="Enter your email address")
+    phone: int = Field(description="Enter your phone number", ge=0, le= 10)
+    dob: str = Field(description="Enter your date of birth")
+
+
